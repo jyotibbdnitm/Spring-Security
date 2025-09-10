1b4812334e28cd97907cd0f7e0dd6f7b100b8f48
Jyoti Chaurasiya <jyotichaurasiya@Jyotis-MacBook-Air.local>
2025-09-11 00:26:46 +0530

    add method level role based security

--- src/main/java/com/example/Spring/Security/Controller.java
+++ src/main/java/com/example/Spring/Security/Controller.java
@@ -1,13 +1,16 @@
 package com.example.Spring.Security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/Security")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public String SecurityImp(){
         return "You have to Secure this Data";
     }
@@ -16,15 +19,21 @@ public class Controller {
     public String SecureDone(){
         return "Your Data is Secured Now";
     }
+
+
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public String admin() {
         return "Hello Admin!";
     }
 
     @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
     public String user() {
         return "Hello User!";
     }
+
+
     @GetMapping("/open")
     public String open() {
         return "This is an open endpoint.";
--- src/main/java/com/example/Spring/Security/SecurityConfig.java
+++ src/main/java/com/example/Spring/Security/SecurityConfig.java
@@ -2,27 +2,31 @@ package com.example.Spring.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .csrf(csrf->csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/open").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/user").hasRole("USER")
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(auth->auth
+                .requestMatchers("/api/open").permitAll()
+                .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic->{});
 
-        return httpSecurity.build();
+                .formLogin(Customizer.withDefaults());
+
+                return httpSecurity.build();
+
     }
 
     @Bean
